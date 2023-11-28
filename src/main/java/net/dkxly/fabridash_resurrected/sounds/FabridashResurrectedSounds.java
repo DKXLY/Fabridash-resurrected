b35c5f72bf7a1c1a130b55ac6c1c175d63cdf3a3
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-11-28 22:56:56 +0100

    This branch should be working now

--- src/main/java/net/dkxly/fabridash_resurrected/sounds/FabridashResurrectedSounds.java
+++ src/main/java/net/dkxly/fabridash_resurrected/sounds/FabridashResurrectedSounds.java
@@ -1,9 +1,10 @@
 package net.dkxly.fabridash_resurrected.sounds;
 
 import net.dkxly.fabridash_resurrected.FabridashResurrectedMod;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import static net.dkxly.fabridash_resurrected.FabridashResurrectedMod.LOGGER;
 
@@ -13,7 +14,7 @@ public class FabridashResurrectedSounds {
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = new Identifier(FabridashResurrectedMod.MOD_ID, name);
-        return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(id));
+        return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
     }
 
     public static void registerSounds() {
